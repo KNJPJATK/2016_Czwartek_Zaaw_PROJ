78151f2b883f2a37154787cc480553d3c3bec602
domino2608 <domino2608@gmail.com>
2016-11-18 23:02:03 +0100

    Added example how to use welcome file and also added some directories for db scripts, css and js.

--- src/main/java/pl/knpj/servlet/Servlet.java
+++ src/main/java/pl/knpj/servlet/Servlet.java
@@ -9,17 +9,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns="/api/*")
+@WebServlet(urlPatterns="/api")
 public class Servlet extends HttpServlet {
 
 	private static final long serialVersionUID = -2842975407425164763L;
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doGet(req, resp);
 		resp.setStatus(HttpServletResponse.SC_OK);
 		PrintWriter pw = resp.getWriter();
-		pw.print("Hello World!");
+		pw.println("Hello World!");
 	}
 
 }
