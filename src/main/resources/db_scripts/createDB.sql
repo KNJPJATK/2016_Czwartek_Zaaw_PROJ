73d0ffd0f48266aba91ba96ec09c5b7eaf5e4e12
Joanna Kolis <joanna.kolis@gmail.com>
2016-11-25 10:50:01 +0100

    Improved createSB.sql script

--- src/main/resources/db_scripts/createDB.sql
+++ src/main/resources/db_scripts/createDB.sql
@@ -1,63 +1,64 @@
-CREATE DATABASE quizowanie;
-USE quizowanie;
-
 CREATE TABLE question (
-	quest_id INT NOT NULL,
+	id INT NOT NULL,
 	title VARCHAR(128) NOT NULL,
-	description VARCHAR(500),
-	PRIMARY KEY (quest_id)
+	description TEXT,
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE answer (
-	ans_id INT NOT NULL,
+	id INT NOT NULL,
 	text VARCHAR(256) NOT NULL,
 	is_correct BOOLEAN NOT NULL,
-	PRIMARY KEY (ans_id)
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE quiz (
-	quiz_id INT NOT NULL,
+	id INT NOT NULL,
 	name VARCHAR(128) NOT NULL,
 	maxTime int NOT NULL,
-	PRIMARY KEY (quiz_id)
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE user (
-	user_id INT NOT NULL,
+CREATE TABLE vuser (
+	id INT NOT NULL,
 	username VARCHAR(64) NOT NULL,
-	password BINARY NOT NULL,
+	password TEXT NOT NULL,
 	user_type CHAR NOT NULL,
-	PRIMARY KEY (user_id),
+	PRIMARY KEY (id),
 	UNIQUE (username)
 );
 
 CREATE TABLE quest_quiz (
-	quest_id INT NOT NULL,
+  	id INT NOT NULL,
+	question_id INT NOT NULL,
 	quiz_id INT NOT NULL,
-	CONSTRAINT quest_guiz_pk PRIMARY KEY (quest_id, quiz_id)
+	CONSTRAINT quest_guiz_pk PRIMARY KEY (id),
+	CONSTRAINT u_question_id_quiz_id UNIQUE (question_id, quiz_id)
 );
 
 CREATE TABLE quest_ans (
-	quest_id INT NOT NULL,
-	ans_id INT NOT NULL,
-	CONSTRAINT quest_ans_pk PRIMARY KEY (quest_id, ans_id)
+  	id INT NOT NULL,
+	question_id INT NOT NULL,
+	answer_id INT NOT NULL,
+	CONSTRAINT quest_ans_pk PRIMARY KEY (id),
+	CONSTRAINT u_question_id_answer_id UNIQUE (question_id, answer_id)
 );
 
-ALTER TABLE quest_quiz 
-	ADD FOREIGN KEY (quest_id) REFERENCES question(quest_id),
-	ADD FOREIGN KEY (quiz_id) REFERENCES quiz(quiz_id);
+ALTER TABLE quest_quiz
+	ADD FOREIGN KEY (question_id) REFERENCES question(id),
+	ADD FOREIGN KEY (quiz_id) REFERENCES quiz(id);
 
 ALTER TABLE quest_ans
-	ADD FOREIGN KEY (quest_id) REFERENCES question(quest_id),
-	ADD FOREIGN KEY (ans_id) REFERENCES answer(ans_id);
+	ADD FOREIGN KEY (question_id) REFERENCES question(id),
+	ADD FOREIGN KEY (answer_id) REFERENCES answer(id);
 
 
-INSERT INTO question VALUES (1, 'What does the fox say?', 'Some description');
+INSERT INTO question(id, title, description) VALUES (1, 'What does the fox say?', 'Some description');
 
-INSERT INTO answer VALUES (1, 'Hatee-hatee-hatee-ho', true);
-INSERT INTO answer VALUES (2, 'Fox does not say anything', false);
+INSERT INTO answer(id, text, is_correct) VALUES (1, 'Hatee-hatee-hatee-ho', true);
+INSERT INTO answer(id, text, is_correct) VALUES (2, 'Fox does not say anything', false);
 
-INSERT INTO quiz VALUES (1, 'Fox quiz', 12);
+INSERT INTO quiz(id, name, maxTime) VALUES (1, 'Fox quiz', 12);
 
-INSERT into quest_ans VALUES (1, 1);
-INSERT into quest_ans VALUES (1, 2);
+INSERT into quest_ans(id, question_id, answer_id) VALUES (1, 1, 1);
+INSERT into quest_ans(id, question_id, answer_id) VALUES (2, 1, 2);
