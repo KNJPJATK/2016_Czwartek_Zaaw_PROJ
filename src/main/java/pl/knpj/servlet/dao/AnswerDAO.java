3c951a2b71429f50a345c2a4807701083358aa7a
Vadim Moskalenko <msVadim@pjwstk.edu.pl>
2016-12-01 12:58:25 +0100

    Add class AnswerDAO, but is not implemented

--- src/main/java/pl/knpj/servlet/dao/AnswerDAO.java
+++ src/main/java/pl/knpj/servlet/dao/AnswerDAO.java
@@ -1,7 +1,10 @@
 package pl.knpj.servlet.dao;
 
+import pl.knpj.servlet.model.Question;
+
 /**
  * Created by vadikms on 29.11.16.
  */
-public class AnswerDAO {
+public class AnswerDAO extends BaseDAO {
+
 }
