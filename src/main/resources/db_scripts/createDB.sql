39091674339354139c261e892270b21229bcc000
Magdalena <pawelec.magdalenam@gmail.com>
2016-12-08 15:59:26 +0100

    SQL corrected. Column is_correct added to table question_answer and shortcuts removed.

--- src/main/resources/db_scripts/createDB.sql
+++ src/main/resources/db_scripts/createDB.sql
@@ -11,13 +11,6 @@ CREATE TABLE answer (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE correct_answer (
-	id INT PRIMARY KEY,
-	q_id INT, 
-	a_id INT,
-	FOREIGN KEY (q_id) REFERENCES question(id),
-	FOREIGN KEY (a_id) REFERENCES answer(id)
-);
 
 CREATE TABLE quiz (
 	id INT NOT NULL,
@@ -35,33 +28,34 @@ CREATE TABLE vuser (
 	UNIQUE (username)
 );
 
-CREATE TABLE quest_quiz (
+CREATE TABLE question_quiz (
   	id INT NOT NULL,
 	question_id INT NOT NULL,
 	quiz_id INT NOT NULL,
-	CONSTRAINT quest_guiz_pk PRIMARY KEY (id),
-	CONSTRAINT u_question_id_quiz_id UNIQUE (question_id, quiz_id)
+	CONSTRAINT question_quiz_pk PRIMARY KEY (id),
+	CONSTRAINT unique_question_id_quiz_id UNIQUE (question_id, quiz_id)
 );
 
-CREATE TABLE quest_ans (
+CREATE TABLE question_answer (
   	id INT NOT NULL,
 	question_id INT NOT NULL,
 	answer_id INT NOT NULL,
-	CONSTRAINT quest_ans_pk PRIMARY KEY (id),
-	CONSTRAINT u_question_id_answer_id UNIQUE (question_id, answer_id)
+	is_correct BOOLEAN NOT NULL,
+	CONSTRAINT question_answer_pk PRIMARY KEY (id),
+	CONSTRAINT unique_question_id_answer_id UNIQUE (question_id, answer_id)
 );
 
-ALTER TABLE quest_quiz
+ALTER TABLE question_quiz
 	ADD FOREIGN KEY (question_id) REFERENCES question(id),
 	ADD FOREIGN KEY (quiz_id) REFERENCES quiz(id);
 
-ALTER TABLE quest_ans
+ALTER TABLE question_answer
 	ADD FOREIGN KEY (question_id) REFERENCES question(id),
 	ADD FOREIGN KEY (answer_id) REFERENCES answer(id);
 
 
 INSERT INTO question(id, title, description) VALUES (1, 'What does the fox say?', 'Some description');
-INSERT INTO question(id, title, description) VALUES (2, 'What is the best programming language?', "Don't think too much!");
+INSERT INTO question(id, title, description) VALUES (2, 'What is the best programming language?', 'Do not think too much!');
 INSERT INTO question(id, title, description) VALUES (3, 'How many primitive data types are in Java?', 'You know it!');
 
 INSERT INTO answer(id, text) VALUES (1, 'Hatee-hatee-hatee-ho');
@@ -77,23 +71,19 @@ INSERT INTO answer(id, text) VALUES (9, '9');
 INSERT INTO quiz(id, name, max_time) VALUES (1, 'Fox quiz', 12);
 INSERT INTO quiz(id, name, max_time) VALUES (2, 'Java quiz', 30);
 
-INSERT into quest_ans(id, question_id, answer_id) VALUES (1, 1, 1);
-INSERT into quest_ans(id, question_id, answer_id) VALUES (2, 1, 2);
-INSERT into quest_ans(id, question_id, answer_id) VALUES (3, 2, 3);
-INSERT into quest_ans(id, question_id, answer_id) VALUES (4, 2, 4);
-INSERT into quest_ans(id, question_id, answer_id) VALUES (5, 2, 5);
-INSERT into quest_ans(id, question_id, answer_id) VALUES (6, 3, 6);
-INSERT into quest_ans(id, question_id, answer_id) VALUES (7, 3, 7);
-INSERT into quest_ans(id, question_id, answer_id) VALUES (8, 3, 8);
-INSERT into quest_ans(id, question_id, answer_id) VALUES (9, 3, 9);
+INSERT into question_answer(id, question_id, answer_id, is_correct) VALUES (1, 1, 1, true);
+INSERT into question_answer(id, question_id, answer_id, is_correct) VALUES (2, 1, 2, false);
+INSERT into question_answer(id, question_id, answer_id, is_correct) VALUES (3, 2, 3, true);
+INSERT into question_answer(id, question_id, answer_id, is_correct) VALUES (4, 2, 4, false);
+INSERT into question_answer(id, question_id, answer_id, is_correct) VALUES (5, 2, 5, false);
+INSERT into question_answer(id, question_id, answer_id, is_correct) VALUES (6, 3, 6, false);
+INSERT into question_answer(id, question_id, answer_id, is_correct) VALUES (7, 3, 7, false);
+INSERT into question_answer(id, question_id, answer_id, is_correct) VALUES (8, 3, 8, true);
+INSERT into question_answer(id, question_id, answer_id, is_correct) VALUES (9, 3, 9, false);
 
-INSERT into correct_answer(id, q_id, a_id) VALUES (1, 1, 1);
-INSERT into correct_answer(id, q_id, a_id) VALUES (2, 2, 3);
-INSERT into correct_answer(id, q_id, a_id) VALUES (3, 3, 8);
-
-INSERT INTO quest_quiz (id, question_id, quiz_id) VALUES (1, 1, 1);
-INSERT INTO quest_quiz (id, question_id, quiz_id) VALUES (2, 2, 2);
-INSERT INTO quest_quiz (id, question_id, quiz_id) VALUES (3, 3, 2);
+INSERT INTO question_quiz (id, question_id, quiz_id) VALUES (1, 1, 1);
+INSERT INTO question_quiz (id, question_id, quiz_id) VALUES (2, 2, 2);
+INSERT INTO question_quiz (id, question_id, quiz_id) VALUES (3, 3, 2);
 
 
 
